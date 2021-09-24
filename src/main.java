bfe66f3a547fa9f3439e6a0f342796aa89371220
winter <180802940@qq.com>
2021-09-24 15:59:13 +0800

    update dayInfor

--- src/main.java
+++ src/main.java
@@ -3,12 +3,29 @@ import Product.Product;
 import Recorder.Recorder;
 import  GUI.*;
 
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class main {
-    public void dayInfor(ArrayList<Product> arrayList){
-
-
+    public static void dayInfor(Recorder recorder){
+        Scanner sn = new Scanner(System.in);
+        try{
+            System.out.println("Please enter year:");
+            int year = sn.nextInt();
+            System.out.println("Please enter month:");
+            int month = sn.nextInt();
+            System.out.println("Please enter day:");
+            int day = sn.nextInt();
+            for(Product product : recorder.searchDay(new int[] {year,month,day}).getRecorder()){
+                product.information();
+                System.out.println();
+            }
+            sn.close();
+        }catch (InputMismatchException e){
+            System.out.println("you have to input integer");
+        }
     }
 
     public static void main(String args[]){
@@ -18,7 +35,7 @@ public class main {
         recorder.add(f);
         recorder.add(f1);
 
-
+        dayInfor(recorder);
         //GUI gui = new GUI();
     }
 }
